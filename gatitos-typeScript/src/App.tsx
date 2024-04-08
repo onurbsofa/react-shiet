a2446e445e53657458ab3349b18a9d55daea1524
BoFo <brunofazio466@gmail.com>
2024-04-07 23:06:08 -0300

    app gatitos base

--- gatitos-typeScript/src/App.tsx
+++ gatitos-typeScript/src/App.tsx
@@ -4,19 +4,30 @@ import axios from 'axios'
 
 
 const RANDOM_FACT_URL = 'https://catfact.ninja/fact'
-
+const CAT_IMAGE_URL = 'https://cataas.com'
 
 function App() {
   
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
   useEffect(() =>{
           axios.get(RANDOM_FACT_URL)
               .then(response => {
+
+                  const { fact } = response.data
                   setFact(response.data)
+                  const ThreeWords = fact.split(' ',3).join(' ')
+                  console.log(ThreeWords)
+
+                  const url = `https://cataas.com/cat/says/${ThreeWords}?size=50&color=red`;
+                  setImageUrl(url);
               })
               .catch(error => {
                 console.error('Error fetching cat fact', error);
               });
+
+
       },[]
   )   
 
@@ -27,6 +38,7 @@ function App() {
 
     <h1>App de Gatitos</h1>
     <p>{fact?.fact}</p>
+    {imageUrl && <img src={imageUrl} alt="cat" />}
 
 
     </main>
